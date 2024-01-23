16a5b639f9b229c3c060b741c54be34abbe266e0
ivanstgit <100668917+ivanstgit@users.noreply.github.com>
2024-01-23 14:46:46 +0300

    lesson 9 (#9)
    Co-authored-by: Ivan St <ivanstgit>

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -14,13 +14,32 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from rest_framework import permissions
 from rest_framework.authtoken import views as AuthtokenViews
 from rest_framework.routers import DefaultRouter
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 from todoapp.views import ProjectModelViewSet, ToDoModelViewSet
 from userapp.views import CustomUserModelViewSet
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Library",
+        default_version="2.1",
+        description="Documentation to out project",
+        contact=openapi.Contact(email="admin@admin.local"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 router = DefaultRouter()
 router.register("users", CustomUserModelViewSet)
@@ -30,9 +49,26 @@ router.register("todos", ToDoModelViewSet)
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
-    path("api/", include(router.urls)),
+    re_path(r"^api/v(?P<version>(1.1|2.1))/", include(router.urls)),
+    # re_path(r"^api/v(?P<version>\d\.\d)/", include(router.urls)),
+    # path("api/", include(router.urls)),
     path("api-token-auth/", AuthtokenViews.obtain_auth_token),
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("api/token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+    re_path(
+        r"^swagger/v(?P<version>(1.1|2.1))/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    re_path(
+        r"^swagger/v(?P<version>(1.1|2.1))(?P<format>\.json|\.yaml)$",
+        schema_view.without_ui(cache_timeout=0),
+        name="schema-json",
+    ),
+    re_path(
+        r"^redoc/v(?P<version>(1.1|2.1))",
+        schema_view.with_ui("redoc", cache_timeout=0),
+        name="schema-redoc",
+    ),
 ]
--- backend/userapp/views.py
+++ backend/userapp/views.py
@@ -2,7 +2,7 @@ from rest_framework import mixins
 from rest_framework.viewsets import GenericViewSet
 
 from .models import CustomUser
-from .serializers import CustomUserModelSerializer
+from .serializers import CustomUserModelSerializer, CustomUserModelSerializerExt
 
 
 class CustomUserModelViewSet(
@@ -15,3 +15,8 @@ class CustomUserModelViewSet(
     queryset = CustomUser.objects.all()
     serializer_class = CustomUserModelSerializer
     # pagination_class = PageNumberPagination
+
+    def get_serializer_class(self):
+        if str(self.request.version).startswith("2."):
+            return CustomUserModelSerializerExt
+        return CustomUserModelSerializer
